1f4daca41a5bad6c6ba7a29fac161aa392783ede
Hwj <hwj@HwjdeMacBook-Pro.local>
2017-03-06 10:00:18 +0800

    UI init

--- src/js/UI/left.js
+++ src/js/UI/left.js
@@ -1,38 +1,4 @@
 /**
  * @author   milan(white gourd angel)
- * @describe UI主类，用于搭建平台页面、事件绑定
+ * @describe 页面左侧操作条
  */
-import './main.less';
-import Vue from 'vue';
-import { Button, Menu, MenuItem, Submenu } from 'element-ui';
-import 'mint-ui/lib/style.css';
-
-Vue.component(Button.name, Button);
-Vue.component('el-menu', Menu);
-Vue.component('el-menu-item', MenuItem);
-Vue.component('el-submenu', Submenu);
-
-console.info(Submenu);
-class Main {
-    constructor() {
-        console.info('UI init');
-        new Vue({
-            el: '.head',
-            template: `<div>
-            <el-menu theme="dark"  class="el-menu-demo" mode="horizontal" >
-              <el-menu-item index="1">处理中心</el-menu-item>
-              <el-submenu index="2">
-                <template slot="title">我的工作台</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
-                <el-menu-item index="2-3">选项3</el-menu-item>
-              </el-submenu>
-              <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-            </div>
-        `
-        });
-    }
-    render(data) {
-    }
-}
-export default () => new Main ();
--- src/js/UI/main.js
+++ src/js/UI/main.js
@@ -4,6 +4,7 @@
  */
 import './main.less';
 import Vue from 'vue';
+import pageHead from './head';
 import { Button, Menu, MenuItem, Submenu } from 'element-ui';
 
 Vue.component('el-menu', Menu);
@@ -12,36 +13,32 @@ Vue.component('el-submenu', Submenu);
 
 class Main {
     constructor() {
-        console.info('UI init');
         new Vue({
             el: '.main',
+            components: {
+                pageHead
+            },
             template: `
             <div>
-            <!-- 顶部菜单 -->
-            <div class="head">
-                <el-menu theme="dark" class="el-menu-demo" mode="horizontal">
-                  <el-menu-item index="1">处理中心</el-menu-item>
-                  <el-submenu index="2">
-                    <template slot="title">我的工作台</template>
-                    <el-menu-item index="2-1">选项1</el-menu-item>
-                    <el-menu-item index="2-2">选项2</el-menu-item>
-                    <el-menu-item index="2-3">选项3</el-menu-item>
-                  </el-submenu>
-                  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-                </el-menu>
-            </div>
+                <!-- 顶部菜单 -->
+                <div class="head">
+                    <pageHead></pageHead>
+                </div>
                 <!-- 内容区域 -->
                 <div class="content">
                     <!-- 左侧操作栏 -->
-                    <div class="left-content">
-                    </div>
+                    <div class="left-content"></div>
                     <!-- 主画板 -->
                     <div class="main-content"></div>
                 </div>
-            </div>`
+                <!-- 底部时间轴 -->
+                <div class="bottom-area"></div>
+            </div>
+            `
         });
     }
     render(data) {
     }
 }
 export default () => new Main ();
+
